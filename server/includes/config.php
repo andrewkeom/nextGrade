980367b6f02c78855fb3075f87cf1abf23e2f17f
Andrew <andrew.keom@gmail.com>
2026-08-27 01:22:35 +0300

    database configuration file

---
+++ server/includes/config.php
@@ -0,0 +1,11 @@
+<?php
+ini_set("display_errors", "1");
+error_reporting(E_ALL);
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "nextgrade_db");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_CHARSET", "utf8mb4");
+
+require_once __DIR__ . "/config.local.php";
